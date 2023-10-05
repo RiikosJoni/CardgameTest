a965240e7464cd9a21785c2cceb6aafa5c9b8b81
JoniRiikonen <riikosjoni08@gmail.com>
2023-10-05 19:53:28 +0300

    Updates things

--- Cardgame_Test/src/App.jsx
+++ Cardgame_Test/src/App.jsx
@@ -3,14 +3,36 @@ import "./App.css"
 
 const playerCard = {
   image: 'https://placebeard.it/240x200',
-  stats: [{name:'Rudeness: ', value: 15},{name:'Pollution: ', value: 30},{name:'Destruction: ', value: 30},{name:'Weight: ', value: 60},{name:'Rarity: ', value: 'Extra Rare'}]
+  stats: [{name:'Rudeness: ', value: 15},
+          {name:'Pollution: ', value: 30},
+          {name:'Destruction: ', value: 30},
+          {name:'Weight: ', value: 60},
+          {name:'Rarity: ', value: 'Extra Rare'}]
+}
+
+const enemyCard = {
+  image: 'https://placebeard.it/240x200',
+  stats: [{name:'Rudeness: ', value: 15},
+          {name:'Pollution: ', value: 30},
+          {name:'Destruction: ', value: 30},
+          {name:'Weight: ', value: 60},
+          {name:'Rarity: ', value: 'Extra Rare'}]
 }
 
 export default function App(){
+
+  function compareCards(){
+    console.log("comparison started!")
+  }
+
   return(
     <>
       <h1>Gary-Cards.</h1>
-      <Card card={playerCard}/>
+      <div className='game'>
+        <Card card={playerCard}/>
+        <button onClick={compareCards} type="button">Play</button>
+        <Card card={enemyCard}/>
+      </div>
     </>
   );
 }
